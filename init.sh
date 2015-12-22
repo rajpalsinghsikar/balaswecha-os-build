6313489d3271089b506efb6a496ae2c486d9e0cb
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-12-22 12:34:53 +0530

    Build the OS

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 WORKDIR=livecdtmp
 FILENAME=ubuntu-10.04-amd64.iso
+IMAGE_NAME=balaswecha-10.04-amd64.iso
 
 mkdir -p $WORKDIR
 cd $WORKDIR
@@ -23,3 +24,18 @@ sudo umount /sys
 sudo umount /dev/pts
 exit
 sudo umount edit/dev
+chmod +w extract-cd/casper/filesystem.manifest
+sudo su
+chroot edit dpkg-query -W --showformat='${Package} ${Version}\n' > extract-cd/casper/filesystem.manifest
+exit
+sudo cp extract-cd/casper/filesystem.manifest extract-cd/casper/filesystem.manifest-desktop
+sudo sed -i '/ubiquity/d' extract-cd/casper/filesystem.manifest-desktop
+sudo sed -i '/casper/d' extract-cd/casper/filesystem.manifest-desktop
+sudo mksquashfs edit extract-cd/casper/filesystem.squashfs -comp xz -e edit/boot
+sudo su
+printf $(du -sx --block-size=1 edit | cut -f1) > extract-cd/casper/filesystem.size
+exit
+cd extract-cd
+sudo rm md5sum.txt
+find -type f -print0 | sudo xargs -0 md5sum | grep -v isolinux/boot.cat | sudo tee md5sum.txt
+sudo mkisofs -D -r -V "$IMAGE_NAME" -cache-inodes -J -l -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o "../$IMAGE_NAME" .
